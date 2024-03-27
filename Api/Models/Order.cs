7f903f86120ee4eb06d0a56ef90d10cd88c894fd
grzesiekjaczewski <grzesiek.jaczewski@gmail.com>
2024-03-27 14:03:47 +0000

    Separate model classes

--- Api/Models/Order.cs
+++ Api/Models/Order.cs
@@ -11,28 +11,5 @@
         public decimal OrderTotal { get; set; }
 
         public List<OrderProduct> OrderProducts { get; set; }
-
-    }
-
-
-    public class OrderProduct
-    {
-        public int OrderId { get; set; }
-
-        public int ProductId { get; set; }
-
-        public Product Product { get; set; }
-    
-        public int Quantity { get; set; }
-
-        public decimal Price { get; set; }
-
-    }
-
-    public class Product
-    {
-        public string Name { get; set; }
-
-        public decimal Price { get; set; }
     }
 }
